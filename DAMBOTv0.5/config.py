cdd4c3e9fab9b87c07656851665d1a607f893fdd
Harold <harold.goldensky@gmail.com>
2020-10-10 09:28:06 +0800

    Add files via upload

---
+++ DAMBOTv0.5/config.py
@@ -0,0 +1,14 @@
+#Login Details
+email = 'sample@gmail.com'
+password = "Sample12345"
+itemUrl = 'https://www.titan22.com/nike-air-force-1-07-craft-black-white.html'
+itemSize = '10'
+
+
+#Payment Details
+card_Number = '5188690464584111'
+card_Holder_name = 'sample sample'
+card_Cvv= '123'
+card_Bank_name = 'HSBC'
+card_Month='01'
+card_Year='2022'
