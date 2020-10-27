01ce70685a997ec8ecbad1fa93155f0485a8c0ba
Harold <harold.goldensky@gmail.com>
2020-10-27 11:29:56 +0800

    Clean codes by adding Web Element Path to Config

--- DAMBOTv0.5/config.py
+++ DAMBOTv0.5/config.py
@@ -1,14 +1,74 @@
-#Login Details
-email = 'email'
-password = "password"
-itemUrl = 'product_link'
-itemSize = 'size'
 
+
+# ==== Don't Change Anything Here ==== #
+#configuration
+chromePath = 'C:\Program Files (x86)\chromedriver.exe'
+extensionPath = 'bfgblailifedppfilabonohepkofbkpm.crx'
+userAgentPath = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+extensionUrlPath = 'chrome-extension://bfgblailifedppfilabonohepkofbkpm/index.html'
+
+#QueBypassExtension
+newRule = '/html/body/app-root/div/div[1]/app-rule-groups/div/div[1]/ul[1]/li/button'
+toggle = '/html/body/app-root/div/nav/div/ul/li[4]/app-toggle/ng-toggle/span/span[3]'
+title = '/html/body/app-root/div/div[1]/app-rule-groups/div/div[2]/app-edit-rule-group/div/div/div[1]/div/div[1]/div[1]/input[1]'
+titanBypass = 'TitanBypass'
+queueit = '/html/body/app-root/div/div[1]/app-rule-groups/div/div[2]/app-edit-rule-group/div/div/div[2]/div/ul/li/div/div[1]/div/div[3]/input'
+queueitBypass = 'https://titan22.queue-it.net'
+redirectTo = '/html/body/app-root/div/div[1]/app-rule-groups/div/div[2]/app-edit-rule-group/div/div/div[2]/div/ul/li/div/div[2]/div/table/tbody/tr/td[1]/ng-select/div/div/div[2]'
+clickRedirect = "//*[contains(text(), 'Redirect To')]"
+titanPage = '/html/body/app-root/div/div[1]/app-rule-groups/div/div[2]/app-edit-rule-group/div/div/div[2]/div/ul/li/div/div[2]/div/table/tbody/tr/td[2]/input'
+titanPageCheckout = 'https://www.titan22.com/checkout'
+saveBypass = '/html/body/app-root/div/nav/div/ul/li[3]/button'
+
+#WaitingTime
+waitingTime = 19000
+
+#SelectingSize 
+qtyId = 'qty'
+quantity = '1'
+sizeId = 'attribute139'
+itemSize = '10'
+productToCart = 'product-addtocart-button'
+continueShopping = 'ajaxcart_cancel'
+
+#checkoutPage 
+creditId = 'ccpp'
+shippingId = 's_method_freeshipping_freeshipping'
+placeOrderId = "//BUTTON[@class='action primary checkout amasty']"
+
+#TitanUrl 
+itemUrl = 'https://www.titan22.com/nike-lebron-18-multicolor.html'
+loginUrl = 'https://www.titan22.com/customer/account/login/'
+checkoutPage = 'https://www.titan22.com/checkout/'
+
+#Login Details
+emailId = 'email'
+passId = 'pass'
+loginButton = 'send2'
+
+email = 'sample@gmail.com'
+password = "Sample12345"
 
 #Payment Details
-card_Number = 'card_number'
-card_Holder_name = 'card_holder'
-card_Cvv= 'cvv'
-card_Bank_name = 'bank_name'
-card_Month='card_month'
-card_Year='card_year'
+cardId = 'credit_card_number'
+cardNameId = 'credit_card_holder_name'
+cvvId = 'credit_card_cvv'
+bankNameId = 'credit_card_issuing_bank_name'
+cardMonthId = 'credit_card_expiry_month'
+cardYearId = 'credit_card_expiry_year'
+submitId = 'btnCCSubmit'
+orderId = '//*[@id="payment-order"]/span'
+
+card_Number = '123456789'
+card_Holder_name = 'sample sample'
+card_Cvv= '132'
+card_Bank_name = 'BDO'
+card_Month='01'
+card_Year='2022'
+
+#ForDiscord
+imageId = '//*[@id="magnifier-item-0"]'
+imageSrc =  'src'
+skuPath = '//*[@id="maincontent"]/div[3]/div/div[1]/div[1]/div[2]/div[6]'
+price = 'price'
+productName = 'base'
